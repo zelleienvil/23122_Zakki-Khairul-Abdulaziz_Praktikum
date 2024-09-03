ed7a3c74688c80e67eb35fa4a574d360dea95e83
zelleienvil <147892066+zelleienvil@users.noreply.github.com>
2024-09-03 16:15:42 +0700

    Add files via upload

---
+++ 23122_Zakki Khairul Abdulaziz_Praktikum1/src/pkg23122_zakki/khairul/abdulaziz_praktikum1/MahasiswaBeraksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23122_zakki.khairul.abdulaziz_praktikum1;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa zakki = new Mahasiswa("Zakki");
+        
+        zakki.membaca();
+        zakki.nyontek();
+        zakki.modifikasi();
+    }
+    
+}
